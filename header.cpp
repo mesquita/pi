7619c8138654c0a18f05e9c8fc54488de295474d
mesquita <mesquuita@gmail.com>
2017-03-26 10:38:44 -0300

    header para os projetos

---
+++ header.cpp
@@ -0,0 +1,19 @@
+/* Copyright (C) - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* 
+* Written by: Bruno G. <brunogranato@poli.ufrj.br>
+*             Nicholas Q. <nicholasquagliani@poli.ufrj.br>
+*             Renata P. <r.baptista@poli.ufrj.br>
+*             Vinicius M. <viniciusmesquita@poli.ufrj.br>
+* 
+* Year: 2017
+* Semester: 1st
+*
+* Electronic and Computing Engineering Department (DEL)
+* Polytechnic School (Poli)
+* Federal University of Rio de Janeiro (UFRJ)
+*
+* Course: PI 
+* Professor: Casé & Joarez
+*/
